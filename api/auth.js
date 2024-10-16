8c4ed8916426a3483e6c18bcfbf86ba814aed562
Aliyah M Smith <aliyahms@umich.edu>
2024-10-16 13:27:04 -0400

    Initialized customer accounts via login or register in database. Configured the enviroment. Created the auth router.

--- api/auth.js
+++ api/auth.js
@@ -2,8 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 // TODO: Import jwt and JWT_SECRET
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // TODO: createToken
+function createToken(id) {
+  // Note: id is wrapped in an object to prevent jwt from coercing it into a string
+  // payload, key, parameter
+  return jwt.sign({ id }, JWT_SECRET, { expiresIn: "1d" });
+}
 
 const prisma = require("../prisma");
 
@@ -16,11 +23,49 @@ router.use(async (req, res, next) => {
   if (!token) return next();
 
   // TODO: Find customer with ID decrypted from the token and attach to the request
+  try {
+    const { id } = jwt.verify(token, JWT_SECRET); // Use jwt.verify with JWT_SECRET to get the id from the token.
+    const customer = await prisma.customer.findUniqueOrThrow({
+      where: { id }, // Find the customer with that id.
+    });
+    req.customer = customer; // Set req.customer to that customer.
+    next(); // Continue to the next middleware.
+  } catch (e) {
+    next(e);
+  }
 });
 
-// TODO: POST /register
+// TODO: POST /register route
+router.post("/register", async (req, res, next) => {
+  // define/state the properties email and password from the request body, so we can pass it to our register customer method
+  const { email, password } = req.body;
+  try {
+    const customer = await prisma.customer.register(email, password); // Save the returned customer in a variable named customer
+    // Pass the id of that customer into createToken, which we defined earlier
+    // Save the returned token in a variable named token
+    const token = createToken(customer.id);
+    res.status(201).json({ token }); // Respond with { token } and a status of 201.
+  } catch (e) {
+    next(e);
+  }
+});
 
-// TODO: POST /login
+// TODO: POST /login route
+router.post("/login", async (req, res, next) => {
+  // Pass the email and password into prisma.customer.login.
+  // Save the returned customer in a variable named customer.
+  const { email, password } = req.body;
+  try {
+    const customer = await prisma.customer.login(email, password);
+    // Pass the id of that customer into createToken
+    // Save the returned token in a variable named token.
+    const token = createToken(customer.id);
+    // Respond with { token }.
+    res.json({ token });
+  } catch (e) {
+    next(e);
+  }
+});
 
 /** Checks the request for an authenticated customer. */
 function authenticate(req, res, next) {
--- api/reservations.js
+++ api/reservations.js
@@ -8,6 +8,15 @@ const { authenticate } = require("./auth");
 
 router.get("/", authenticate, async (req, res, next) => {
   // TODO: Send reservations made by the logged in customer
+  try {
+    const reservations = await prisma.reservation.findMany({
+      where: { customerId: req.customer.id }, // Send all of the reservations made by the customer stored in req.customer.
+      include: { restaurant: true }, // Include the restaurant of each reservation.
+    });
+    res.json(reservations);
+  } catch (e) {
+    next(e);
+  }
 });
 
 // TODO: POST /
