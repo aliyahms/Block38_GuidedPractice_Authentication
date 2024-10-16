082d7909cad6580bc749ebf9d77f9ed756e003f7
Aliyah M Smith <aliyahms@umich.edu>
2024-10-16 13:40:10 -0400

    Completed routers. Still need to edit notes

--- api/reservations.js
+++ api/reservations.js
@@ -6,6 +6,8 @@ const prisma = require("../prisma");
 // Notice we use {} when importing `authenticate` because it is not the only export
 const { authenticate } = require("./auth");
 
+// GET / ROUTE
+// It should only be accessible to a customer that is logged in.
 router.get("/", authenticate, async (req, res, next) => {
   // TODO: Send reservations made by the logged in customer
   try {
@@ -19,4 +21,23 @@ router.get("/", authenticate, async (req, res, next) => {
   }
 });
 
-// TODO: POST /
+// TODO: POST / route
+// It should only be accessible to a customer that is logged in.
+router.post("/", authenticate, async (req, res, next) => {
+  // It will create a new reservation under the logged in customer,
+  //according to the partySize and restaurantId specified in the request body.
+  const { partySize, restaurantId } = req.body;
+  try {
+    const reservation = await prisma.reservation.create({
+      data: {
+        partySize: +partySize,
+        restaurantId: +restaurantId,
+        customerId: req.customer.id,
+      },
+    });
+    // It then sends the newly created reservation with status 201.
+    res.status(201).json(reservation);
+  } catch (e) {
+    next(e);
+  }
+});
